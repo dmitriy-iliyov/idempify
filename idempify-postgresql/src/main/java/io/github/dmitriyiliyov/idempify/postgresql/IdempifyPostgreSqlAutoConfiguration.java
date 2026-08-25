617c790702aa29d06d69052008c02cd3f955ccc5
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-08-25 16:18:03 +0200

    refactor(postgresql autoconfig honours idempify.enabled & its pom leans on the parent)

--- idempify-postgresql/src/main/java/io/github/dmitriyiliyov/idempify/postgresql/IdempifyPostgreSqlAutoConfiguration.java
+++ idempify-postgresql/src/main/java/io/github/dmitriyiliyov/idempify/postgresql/IdempifyPostgreSqlAutoConfiguration.java
@@ -1,13 +1,22 @@
 package io.github.dmitriyiliyov.idempify.postgresql;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.simple.JdbcClient;
 
 @AutoConfiguration
+@ConditionalOnProperty(
+        prefix = "idempify",
+        name = "enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
 public class IdempifyPostgreSqlAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public PostgreSqlTransactionalOperationRepository idempifyPostgreSqlOperationRepository(JdbcClient jdbcClient) {
         return new PostgreSqlTransactionalOperationRepository(jdbcClient);
     }
